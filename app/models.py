f67e22affac6b512c7d5bdd0fdd4cd9fb08848e6
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-21 22:35:44 +0300

    changes of file structure

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+
+
+
 class Review:
 
     all_reviews = []
@@ -12,10 +27,10 @@ class Review:
     def save_review(self):
         Review.all_reviews.append(self)
 
+
     @classmethod
     def clear_reviews(cls):
         Review.all_reviews.clear()
-
 
     @classmethod
     def get_reviews(cls,id):
@@ -27,4 +42,3 @@ class Review:
                 response.append(review)
 
         return response
-   
