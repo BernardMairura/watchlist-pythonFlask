65ef68340817e0c8b06d4b2e82bd39c6742b0f9f
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-24 10:31:06 +0300

    python3.6 manage.py db init

--- manage.py
+++ manage.py
@@ -2,16 +2,21 @@ from app import create_app,db
 from flask_script import Manager,Shell,Server
 
 
-from app.models import User
+from app.models import User,Role
+
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+migrate=Migrate(app,db)
+
 
 manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
 
-manager.add_command('server',Server)
+
 @manager.command
 def test():
     """Run the unit tests."""
@@ -22,7 +27,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db, User = User)
+    return dict(app = app,db = db, User = User, Role=Role)
 
 if __name__ == '__main__':
     manager.run()
